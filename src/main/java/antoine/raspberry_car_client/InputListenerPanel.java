5bf9080e977072758f797bcd7669e561840d0cb7
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-10-04 13:10:35 +0200

    input listener works

--- src/main/java/antoine/raspberry_car_client/InputListenerPanel.java
+++ src/main/java/antoine/raspberry_car_client/InputListenerPanel.java
@@ -1,31 +1,48 @@
 package antoine.raspberry_car_client;
 
-import antoine.raspberry_car_client.web.client.WebSocketConnection;
 import java.awt.event.ActionEvent;
+import java.util.function.Consumer;
 import javax.swing.AbstractAction;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationContext;
 
 @Slf4j
 public class InputListenerPanel extends JPanel {
 
-    public InputListenerPanel(ApplicationContext app) {
-        var socket = app.getBean(WebSocketConnection.class);
+    // public InputListenerPanel(ApplicationContext app) {
 
-        getInputMap().put(KeyStroke.getKeyStroke("Z"), "moveForward");
+    //     getInputMap().put(KeyStroke.getKeyStroke("Z"), "moveForward");
 
-        getActionMap()
-            .put(
-                "moveForward",
-                new AbstractAction() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        log.info("Z");
-                        socket.send(100, true);
-                    }
-                }
-            );
-    }
+    //     getActionMap()
+    //         .put(
+    //             "moveForward",
+    //             new AbstractAction() {
+    //                 @Override
+    //                 public void actionPerformed(ActionEvent e) {
+    //                     log.info("Z");
+    //                     socket.send(100, true);
+    //                 }
+    //             }
+    //         );
+    // }
+
+    // public void registerInput(
+    //     String name,
+    //     KeyStroke key,
+    //     Consumer<ActionEvent> action
+    // ) {
+    //     getInputMap().put(key, name);
+
+    //     getActionMap()
+    //         .put(
+    //             name,
+    //             new AbstractAction() {
+    //                 @Override
+    //                 public void actionPerformed(ActionEvent e) {
+    //                     action.accept(e);
+    //                 }
+    //             }
+    //         );
+    // }
 }
--- src/main/java/antoine/raspberry_car_client/MainFrame.java
+++ src/main/java/antoine/raspberry_car_client/MainFrame.java
@@ -1,11 +1,19 @@
 package antoine.raspberry_car_client;
 
+import antoine.raspberry_car_client.web.client.WebSocketConnection;
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 
+@Slf4j
 public class MainFrame extends JFrame {
 
     public MainFrame(ApplicationContext app) {
+        var socket = app.getBean(WebSocketConnection.class);
+
         setTitle("raspberry-car-client");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -14,6 +22,34 @@ public class MainFrame extends JFrame {
         setLocationRelativeTo(null);
         setVisible(true);
 
-        getContentPane().add(new InputListenerPanel(app));
+        var inputs = new InputListenerPanel();
+        inputs.addKeyListener(
+            new KeyListener() {
+                @Override
+                public void keyTyped(KeyEvent e) {
+                    // DO NOTHING
+                }
+
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    log.info("pressed");
+                    if (e.getKeyChar() == 'Z') {
+                        socket.send(100, true);
+                    }
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e) {
+                    log.info("released");
+                    if (e.getKeyChar() == 'Z') {
+                        socket.send(100, true);
+                    }
+                }
+            }
+        );
+        inputs.setBackground(Color.BLUE);
+        inputs.setFocusable(true);
+        // inputs.u
+        getContentPane().add(inputs);
     }
 }
